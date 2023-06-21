f52b72eff5373398e18eddc301160da7de2706ff
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-06-21 15:44:20 -0600

    Add handleClearCompleted function

--- tests/taskFunctions.test.js
+++ tests/taskFunctions.test.js
@@ -66,3 +66,61 @@ describe('handleNewTask function', () => {
     expect(todoList.children[0].textContent).toContain('New task');
   });
 });
+
+describe('handleClearCompleted function', () => {
+  it('should remove completed tasks', () => {
+    document.body.innerHTML = `
+      <div class="container">
+          <header>
+              <h1>Today's to Do</h1>
+              <button id="refresh-btn"><i class="fas fa-sync-alt"></i></button>
+          </header>
+          <div class="input-container">
+              <input type="text" id="task-input" placeholder="Add to your list..." />
+              <button id="enter-btn"><i class="fas fa-arrow-right"></i></button> <!-- NUEVO: botón Enter -->
+          </div>
+          <ul id="todo-list">
+              <li>
+                  <div class="text-container">
+                      <input type="checkbox" checked>
+                      <span>Completed Task</span>
+                  </div>
+              </li>
+              <li>
+                  <div class="text-container">
+                      <input type="checkbox">
+                      <span>Incomplete Task</span>
+                  </div>
+              </li>
+          </ul>
+          <button id="clear-completed-btn">Clear all completed</button>
+      </div>
+      `;
+
+    const todoList = document.getElementById('todo-list');
+
+    // Mock getTasks function to return tasks from DOM
+    const getTasks = () => {
+      const tasks = [];
+      const listItems = Array.from(todoList.children);
+
+      listItems.forEach((item, index) => {
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        const span = item.querySelector('span');
+
+        tasks.push({
+          description: span.textContent,
+          completed: checkbox.checked,
+          index,
+        });
+      });
+
+      return tasks;
+    };
+
+    handleClearCompleted(populateTodoList, getTasks, setTasks);
+
+    expect(todoList.children.length).toBe(1);
+    expect(todoList.children[0].textContent).not.toContain('Completed Task');
+  });
+});
